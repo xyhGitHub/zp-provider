942dd6a978e9b68ffa0d82c0ce29dc0c36ce2634
xyhGitHub <839174810@qq.com>
2018-05-28 10:08:04 +0800

    日志记录

--- spring-boot-utils/src/main/java/com/four/model/Logs.java
+++ spring-boot-utils/src/main/java/com/four/model/Logs.java
@@ -1,18 +1,24 @@
 package com.four.model;
 
-import org.springframework.data.annotation.Id;
+
+
 
 import java.io.Serializable;
 
 public class Logs implements Serializable{
 	private static final long serialVersionUID = 923522888120974690L;
-	@Id
+
 	private String logId;
 	private String clazzName;
 	private String methodName;
 	private String params;
-	
-	
+	private String exception;
+	private String user;
+	private String chenggongcount;
+	private String shibaicount;
+
+
+
 	
 	public String getParams() {
 		return params;
@@ -38,10 +44,47 @@ public class Logs implements Serializable{
 	public void setMethodName(String methodName) {
 		this.methodName = methodName;
 	}
+
+	public String getException() {
+		return exception;
+	}
+	public void setException(String exception) {
+		this.exception = exception;
+	}
+	public String getUser() {
+		return user;
+	}
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	public String getChenggongcount() {
+		return chenggongcount;
+	}
+
+	public void setChenggongcount(String chenggongcount) {
+		this.chenggongcount = chenggongcount;
+	}
+
+	public String getShibaicount() {
+		return shibaicount;
+	}
+
+	public void setShibaicount(String shibaicount) {
+		this.shibaicount = shibaicount;
+	}
+
 	@Override
 	public String toString() {
-		return "Logs [logId=" + logId + ", clazzName=" + clazzName + ", methodName=" + methodName + "]";
+		return "Logs{" +
+				"logId='" + logId + '\'' +
+				", clazzName='" + clazzName + '\'' +
+				", methodName='" + methodName + '\'' +
+				", params='" + params + '\'' +
+				", exception='" + exception + '\'' +
+				", user='" + user + '\'' +
+				", chenggongcount='" + chenggongcount + '\'' +
+				", shibaicount='" + shibaicount + '\'' +
+				'}';
 	}
-	
-	
 }
